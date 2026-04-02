d21aa135c10aeafc48c958e7a7b5439adcd3f066
ngwaradkar <ngwaradkar@gmail.com>
2026-04-02 10:54:05 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Ensure the basePath matches your repository name for github.io deployment
+  basePath: '/nikhil-waradkar',
+  assetPrefix: '/nikhil-waradkar',
 };
 
 export default nextConfig;
